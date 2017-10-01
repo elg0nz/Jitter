d84488c192641d171df5bd757f87f255c9a1a952
Gonzalo Maldonado <elg0nz@gmail.com>
2017-09-30 18:12:56 -0700

    Transition to TweetsViewController after login

--- Jitter/ViewControllers/TweetsViewController.swift
+++ Jitter/ViewControllers/TweetsViewController.swift
@@ -1,36 +1,36 @@
 //
-//  LoginViewController.swift
+//  TweetsViewController.swift
 //  Jitter
 //
-//  Created by Gonzalo Maldonado Martinez on 9/26/17.
+//  Created by Gonzalo Maldonado Martinez on 9/30/17.
 //  Copyright © 2017 WanderTap. All rights reserved.
 //
 
 import UIKit
-import BDBOAuth1Manager
 
-
-
-class LoginViewController: UIViewController {
+class TweetsViewController: UIViewController {
+    var tweets: [Tweet]!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        TwitterClient.sharedInstance.homeTimeline(
+            success: { (tweets: [Tweet]) in
+                self.tweets = tweets
+                tweets.forEach({ (tweet: Tweet) in
+                    print(tweet)
+                })
+            },
+            failure: { (error: NSError) in
+                print(error)
+            }
+        )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func onLoginButton(_ sender: Any) {
-        TwitterClient.sharedInstance.login(success: {
-            print("I've logged in")
-            self.performSegue(withIdentifier: "loginSegue", sender: nil)
-        }) { (error: Error) in
-            print(error.localizedDescription)
-        }
-    }
+    
 
     /*
     // MARK: - Navigation
